927242fbda5a5a2700e131b8f52d463501841eae
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-01-30 19:02:11 -0400

    Criando UserCase - Verificando existencia de Usuario e Criando Usuario

---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,45 @@
+import { client } from "../../prisma/client"
+import { hash } from "bcryptjs"
+
+interface IUserRequest{
+   
+  name : string,
+  username : string,
+  password : string
+}
+
+class CreateUserUseCase {
+
+
+  async execute({name, username, password}: IUserRequest){
+    
+    // [X] - Verificar se o usario existe
+    const userAlreadyExists = await client.user.findFirst({
+      where : {
+        username
+      }
+    });
+
+    if(userAlreadyExists){
+      throw new Error("user and password incorrect")
+    }
+    // [] - Cadastra o Usuario
+      const passwordHash = await hash(password, 8);
+      const user = client.user.create({
+       data : {
+        name,
+        username,
+        password : passwordHash,
+       }
+     })
+
+     return user;
+
+  }
+
+
+}
+
+export { 
+  CreateUserUseCase
+}
