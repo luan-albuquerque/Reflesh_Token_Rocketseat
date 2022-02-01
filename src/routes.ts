679f8cd4a17dcd46940e986d0f86e16099eea3ae
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-02-01 10:26:44 -0400

    Adicionando Middle nas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,26 @@
-import { Router } from "express"
-import { CreateUserController } from "./useCases/CreateUser/CreateUserController";
+import { request, response, Router } from "express"
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { AuthenticateUserController } from "./useCases/authenticateUser/AuthenticateUserController";
+import { CreateUserController } from "./useCases/createUser/CreateUserController";
+
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users",createUserController.handle);
+router.post("/login",authenticateUserController.handle);
+router.get("/courses", ensureAuthenticated, (request, response) =>{
+
+   return response.json([
+     {id:1, name : "Nodejs"},
+     {id:2, name : "React"},
+     {id:3, name : "ReactNative"}, 
+     {id:4, name : "Fluter"} ,
+     {id:5, name : "Elixir"} 
+
+   ])
+})
 
 export { router }
