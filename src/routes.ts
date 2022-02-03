1831abc2f29ad3d3106d662c6419c61a8046a86c
Luan Albuquerque dos Santos <51758832+luan-albuquerque@users.noreply.github.com>
2022-02-03 13:45:37 -0400

    Routas de Reflesh Token

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,18 @@ import { request, response, Router } from "express"
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { AuthenticateUserController } from "./useCases/authenticateUser/AuthenticateUserController";
 import { CreateUserController } from "./useCases/createUser/CreateUserController";
+import { RefleshTokenUserController } from "./useCases/refleshTokenUser/refleshTokenUserController";
 
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
+const refleshTokenUserController = new RefleshTokenUserController();
 
 router.post("/users",createUserController.handle);
 router.post("/login",authenticateUserController.handle);
+router.post("/reflesh-token",refleshTokenUserController.handle);
 router.get("/courses", ensureAuthenticated, (request, response) =>{
 
    return response.json([
